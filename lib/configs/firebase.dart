13cfb4a57f31fcb51c1f9613335671318725b174
Jonatha Junkes <junkes1995@gmail.com>
2021-09-20 23:08:06 -0300

    Firebase Class

---
+++ lib/configs/firebase.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:crypto/crypto.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class Firebase {
+  final dbRef = FirebaseDatabase.instance;
+
+  static const _firebaseUrl = 'https://agendaai-85f9f-default-rtdb.firebaseio.com/';
+
+
+
+  call_child_db(String key){
+    //var db = FirebaseDatabase.instance.reference().child("Users").orderByKey().equalTo(key);
+    var db = FirebaseDatabase.instance.reference().child("Users").orderByChild("senha").equalTo(key);
+    db.once().then((DataSnapshot snapshot){
+      Map<dynamic, dynamic> values = snapshot.value;
+      values.forEach((key,values) {
+        print(key);
+        print(values["nome"]);
+        print(values["email"]);
+        print(values["senha"]);
+      });
+    });
+  }
+
+  Future<void> createNewUser(String cpf,String email,String nome,String telefone, String dataNascimento,String sexo,String senha)async {
+    final response = await http.put(
+      Uri.parse(_firebaseUrl+"Users/$cpf.json"),
+      headers: {
+        'Content-Type' : 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode({
+        'nome': nome,
+        'telefone': telefone,
+        'nascimento': dataNascimento,
+        'sexo': sexo,
+        'email': email,
+        'senha': gerarHASH(senha)
+      }),
+    );
+    //print(response.body);
+  }
+  String gerarHASH(String texto){
+    var bytess = utf8.encode(texto);
+    var hashs = sha256.convert(bytess);
+    return hashs.toString();
+  }
+
+
+}
